1fa8ed37e32c0363a325fc65e56d5918ccf473f8
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-05-11 13:28:39 +0700

    feat: add authz roles for user service

--- common/authorization/roles.go
+++ common/authorization/roles.go
@@ -15,10 +15,18 @@ type AccessibleRoles map[string]map[string][]uint32
 const (
 	BasePath = "tracer_study_grpc"
 	AuthSvc  = "AuthService"
+	UserSvc = "UserService"
 )
 var roles = AccessibleRoles{
 	"/" + BasePath + "." + AuthSvc + "/": {
 		"RegisterUser": {1, 2},
+	},
+	"/" + BasePath + "." + UserSvc + "/": {
+		"GetAllUser": {1, 2},
+		"GetUserById": {1, 2},
+		"CreateUser": {1, 2},
+		"UpdateUser": {1, 2},
+		"DeleteUser": {1, 2},
 	},
 }
 
